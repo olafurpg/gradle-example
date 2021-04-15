25b6f7744c4290bbadf68d40cd8542e21b39059d
Ólafur Páll Geirsson <olafurpg@gmail.com>
2021-04-15 14:43:37 +0200

    WIP

--- j11/src/test/java/example/ExampleTest11.java
+++ j11/src/test/java/example/ExampleTest11.java
@@ -1,3 +1,8 @@
 package example;
 
-public class ExampleTest11 {}
+import org.junit.Assert;
+public class ExampleTest11 {
+    public static void run() {
+        Assert.assertEquals(1, 2);
+    }
+}
